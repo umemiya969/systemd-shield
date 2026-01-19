510c4326f89e1943c2ac654565ef03e249390e4c
umemiya <elanie477@gmail.com>
2026-01-19 12:44:04 +0800

    Update dependency_guard.sh

--- modules/dependency_guard.sh
+++ modules/dependency_guard.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 apply_dependency_guard() {
   systemctl disable NetworkManager-wait-online.service 2>/dev/null || true
   systemctl disable systemd-networkd-wait-online.service 2>/dev/null || true
